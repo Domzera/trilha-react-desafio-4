88f9640ab30c1ab04b1861fe08ffd09c4d87fbff
Domzera <luizceliogf@hotmail.com>
2026-04-09 09:24:28 -0300

    Finalização do desafio de Button disabled comcluido

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<React.ButtonHTMLAttributes<HTMLButtonElement>>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: ${ (props) => props.disabled ? '#ccc' : '#81259D'};
     color: #FFF;
 
     border: 1px solid #81259D;
     border-radius: 21px;
+    
 
     &:hover {
         opacity: 0.6;
         cursor:pointer;
     }
-`
+`;
